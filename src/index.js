1815816b69395892b2881a65784d4af5e8f09152
thiago-ivasaki <thiagoivasaki@gmail.com>
2022-09-23 15:59:15 -0300

    Adicionando ROuter e Provider

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
-  <App />,
+  <BrowserRouter>
+    <Provider store={ store }>
+      <App />
+    </Provider>
+  </BrowserRouter>,
   document.getElementById('root'),
 );
