07c78272d57828c0a68b3a276e4028ef613366ae
João Gomes <panterudo17100@gmail.com>
2022-09-26 18:25:54 -0300

    feat: requisto 16 ao 18

---
+++ src/components/Drinks.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Footer from './Footer';
+
+function Drinks() {
+  return (
+    <div>
+      Drinks
+      <Footer />
+    </div>
+  );
+}
+
+export default Drinks;
