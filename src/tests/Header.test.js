07d18d2611a73e8171285d5ad43dad7aa6473bf4
pguilhermef <pguilhermesantos@hotmail.com>
2022-09-27 17:07:23 -0300

    test: Adiciona casos de teste para os componentes

--- src/tests/Header.test.js
+++ src/tests/Header.test.js
@@ -1,12 +1,21 @@
 import React from 'react';
 // import { screen } from '@testing-library/react';
-import renderWithRouterAndRedux from './helpers/renderWith';
-import Header from '..';
+import { render } from '@testing-library/react';
+import renderWithRouter from './helpers/RenderWith';
+import Header from '../components/Header';
+import App from '../App';
 
 describe('Testa o componente Header', () => {
-  renderWithRouterAndRedux(<Header />);
   test('Testa se o ícone do perfil é rendenizado', () => {
+    render(<Header />);
+  });
 
+  test('Testa se o titulo da pagina é rendenizado de maneira correta', () => {
+    const { history } = renderWithRouter(<App />);
+
+    history.push('/drinks');
+
+    expect(history.location.pathname).toBe('/drinks');
   });
 
   test('Testa se o ícone de pesquisa é rendenizado', () => {
